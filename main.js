4abbf1c649d7a31e2be583da0dbccc44fb314fa5
Eyal-donut <LilaEyal@gmail.com>
2023-02-10 22:25:22 +0100

    11

--- main.js
+++ main.js
@@ -4,27 +4,6 @@
 let chartData = []
 let chartLabels = []
 const lineChart = document.getElementById('myChart');
-let myChart =new Chart(lineChart, {
-    type: "line",
-    data: {
-    labels: chartLabels,
-    datasets: [
-        {
-        label: "population of the countries",
-        data: chartData,
-        borderWidth: 1,
-        },
-    ],
-    },
-    options: {
-    scales: {
-        y: {
-        beginAtZero: true,
-        },
-    },
-    },
-});
-
 
 
 const continentsBtnContainer = document.getElementById('continents-btn-container')
@@ -32,6 +11,7 @@ const countryButtonsGrid = document.getElementById("countries-btn-grid")
 const spinnerContainer = document.getElementById("spinner-container")
 const spinner = document.querySelector(".spinner")
 
+//#################################################################################################################
 //############################################### Aid functions ####################################################
 
 
@@ -47,74 +27,29 @@ const resetChart =() => {
 
 }
 
-
-const setChartVariablesWithCountryPopulation = (data, country) => {
-        const populationCountsArray = data.data.populationCounts
-        const lastIndex = data.data.populationCounts.length -1
-        const populationValue =  data.data.populationCounts[lastIndex].value
-        console.log(populationCountsArray)
-        console.log(populationValue)
-
-        chartLabels.push(`${country}`)
-        chartData.push(populationValue)
-        console.log(chartData)
-        console.log(chartLabels)
-
-}
-
-const setChartVariablesWithCity = (dataInput) => {
-    dataInput.data.forEach(city => {
-        const cityName = city.city
-        console.log(cityName)
-        chartLabels.push(cityName)
-
-        const populationCountsArray = city.populationCounts
-        const populationValue =  city.populationCounts[0].value
-        // console.log(populationCountsArray)
-        // console.log(populationValue)
-
-        chartData.push(populationValue)
-        // console.log(chartData)
-        // console.log(chartLabels)
-
-    })
-}
-
 const initChart = (countries, populationData) => {
-myChart =new Chart(lineChart, {
-        type: "line",
-        data: {
-        labels: countries,
-        datasets: [
-            {
-            label: "population of the countries",
-            data: populationData,
-            borderWidth: 1,
+    myChart =new Chart(lineChart, {
+            type: "line",
+            data: {
+            labels: countries,
+            datasets: [
+                {
+                label: "population of the countries",
+                data: populationData,
+                borderWidth: 1,
+                },
+            ],
             },
-        ],
-        },
-        options: {
-        scales: {
-            y: {
-            beginAtZero: true,
+            options: {
+            scales: {
+                y: {
+                beginAtZero: true,
+                },
             },
-        },
-        },
-    });
-}
-
-
-const initFromLocalStorage = (continent) => {
-    const localDataCountries = JSON.parse(localStorage.getItem(`countriesIn${continent}`));
-        resetChart();
-        getPopulationsForEachCountry(localDataCountries)
-        createCountryButtonsFromData(localDataCountries)
-        initChart(chartLabels, chartData)
-}
-
-//###############################################################################################################
-
-//?###################################### Click on continent: get countries & populations   ####################################################
+            },
+        });
+        myChart.update()
+    }
 
 const createCountryButtonsFromData = (data) => {
     countryButtonsGrid.innerHTML = null
@@ -125,6 +60,51 @@ const createCountryButtonsFromData = (data) => {
         countryButtonsGrid.append(button)
     })
 }
+    
+
+//#######################################################################################################################
+//################################################# Data Massage and Init  ##############################################
+
+const setChartVariablesWithCountryPopulation = (data, country) => {
+        const populationCountsArray = data.data.populationCounts
+        const lastIndex = data.data.populationCounts.length -1
+        const populationValue =  data.data.populationCounts[lastIndex].value
+        chartLabels.push(`${country}`)
+        chartData.push(populationValue)
+}
+
+const setChartVariablesWithCity = (dataInput) => {
+    dataInput.data.forEach(city => {
+        const cityName = city.city
+        chartLabels.push(cityName)
+        const populationCountsArray = city.populationCounts
+        const populationValue =  city.populationCounts[0].value
+        chartData.push(populationValue)
+    })
+}
+
+const initCountriesFromLocalStorage = (continent) => {
+    const localDataCountries = JSON.parse(localStorage.getItem(`countriesIn${continent}`));
+    resetChart();
+    getPopulationsForEachCountry(localDataCountries)
+    createCountryButtonsFromData(localDataCountries)
+    initChart(chartLabels, chartData)
+    // myChart.update()
+
+    
+}
+
+const initCitiesFromLocalStorage = (country) => {
+    const localDataCities = JSON.parse(localStorage.getItem(`citiesIn${country}`));
+    resetChart();
+    setChartVariablesWithCity(localDataCities)
+    initChart(chartLabels, chartData)
+}
+
+//###############################################################################################################
+
+//?###########################################################################################################################
+//?################################################### Fetch information   ####################################################
 
 
 const getCountryPopulation = async (country) => {
@@ -147,12 +127,14 @@ const getCountryPopulation = async (country) => {
                 throw new Error(response.status)
             }
             const populationData = await response.json()
-            localStorage.setItem(`populationOf${country}`, JSON.stringify(await populationData))
-            setChartVariablesWithCountryPopulation(await populationData, country)
+            localStorage.setItem(`populationOf${country}`, JSON.stringify(populationData))
+            const localPopulationData = JSON.parse(localStorage.getItem(`populationOf${country}`));
+
+            setChartVariablesWithCountryPopulation(localPopulationData, country)
 
         } catch (error) {
-            // console.log("Something went wrong")
-            // console.log(error)
+            console.log("Something went wrong")
+            console.log(error)
             
         }
     }
@@ -168,53 +150,38 @@ const getCountriesByContinent = async (continent) =>{
 
     if (localStorage.getItem(`countriesIn${continent}`) !== null) {
 
-        initFromLocalStorage(continent)
+        initCountriesFromLocalStorage(continent)
     }
     else {
         try {
-            // toggleSpinnerView()
+            toggleSpinnerView()
             const response = await fetch(`https://restcountries.com/v3.1/region/${continent}`)
             if(!response.ok) {
                 throw new Error(response)
             }
             const countriesData = await response.json()
-            localStorage.setItem(`countriesIn${continent}`, JSON.stringify(await countriesData))
-            initFromLocalStorage(continent)
+            localStorage.setItem(`countriesIn${continent}`, JSON.stringify(countriesData))
+            initCountriesFromLocalStorage(continent)
+            const localDataCountries = JSON.parse(localStorage.getItem(`countriesIn${continent}`));
+            resetChart();
+            getPopulationsForEachCountry(localDataCountries)
+            createCountryButtonsFromData(localDataCountries)
+            initChart(chartLabels, chartData)
+            toggleSpinnerView()
         } catch (error) {
             console.log("Something went wrong")
             console.log(error)
         }
     }
-
 }
-
-
-document.addEventListener("click", function(e){
-    if(e.target.className === 'continent-btn') {
-        const continent = e.target.innerText.toLowerCase()
-        getCountriesByContinent(continent)
-    }
-})
-
-//?#############################################################################################################################################
-
-
-//################################################ Click on country - get cities populations ###################################################
-
 
 const getCitiesByCountry = async (country) => {
     if (localStorage.getItem(`citiesIn${country}`) !== null) {
-        resetChart();
-        const localDataCities = JSON.parse(localStorage.getItem(`citiesIn${country}`));
-        setChartVariablesWithCity(localDataCities)
-        initChart(chartLabels, chartData)
-
+        initCitiesFromLocalStorage(country)
     }
     else{
-
         try {
             toggleSpinnerView()
-            resetChart();
             const response = await fetch('https://countriesnow.space/api/v0.1/countries/population/cities/filter', {
                 method: 'POST',
                 headers: {
@@ -234,8 +201,8 @@ const getCitiesByCountry = async (country) => {
             const citiesData = await response.json()
             console.log(citiesData)
             localStorage.setItem(`citiesIn${country}`, JSON.stringify(await citiesData))
-            setChartVariablesWithCity(citiesData)
-            initChart(chartLabels, chartData)
+
+            initCitiesFromLocalStorage(country)
             toggleSpinnerView()
 
         } catch (error) {
@@ -244,6 +211,20 @@ const getCitiesByCountry = async (country) => {
         }
     }
 }
+
+
+
+//?#############################################################################################################################################
+
+//##############################################################################################################################################
+//################################################ Event listeners - click ###################################################
+
+document.addEventListener("click", function(e){
+    if(e.target.className === 'continent-btn') {
+        const continent = e.target.innerText.toLowerCase()
+        getCountriesByContinent(continent)
+    }
+})
 
 document.addEventListener("click", function(e){
     if(e.target.className === 'country-btn') {
@@ -254,9 +235,27 @@ document.addEventListener("click", function(e){
 
 //##############################################################################################################################################
 
-localStorage.clear()
 
 
-
+let myChart =new Chart(lineChart, {
+    type: "line",
+    data: {
+        labels: chartLabels,
+        datasets: [
+            {
+            label: "population of the countries",
+            data: chartData,
+            borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+});
 
 
